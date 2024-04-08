da4093e525547e17ff361c720c9904b5924422a8
JoeyHu-smileyao <162651942+JoeyHu-smileyao@users.noreply.github.com>
2024-04-08 16:09:19 +0800

    Update plugin.py

--- src/sentry_dingding/plugin.py
+++ src/sentry_dingding/plugin.py
@@ -59,7 +59,7 @@ class DingDingPlugin(NotificationPlugin):
             "markdown": {
                 "title": title,
                 "text": u"######## {title} \n > {message} [href]({url})".format(
-                    title=str(event),
+                    title=repr(event),
                     message=event.message,
                     url=u"{}events/{}/".format(group.get_absolute_url(), event.id),
                 )
